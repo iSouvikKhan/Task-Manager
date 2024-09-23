2d804e53fb6f46bd2d90f53c1125764cb807086a
souvikkhan <souvik.khan@nagarro.com>
2024-09-23 19:26:43 +0530

    config

--- next-frontend/next.config.mjs
+++ next-frontend/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint checks during the build
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Disable TypeScript errors during the build
+  },
+};
 
 export default nextConfig;
